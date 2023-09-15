6e660cb2b29401a681ece4ab51badef1036391ab
salamer <salamer_gaga@163.com>
2023-09-16 00:43:33 +0800

    fix

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from faster_whisper import WhisperModel
 model_size = "tiny"
-model = WhisperModel(model_size, device="cpu", compute_type="int8", local_files_only=True, download_root="/tmp")
+model = WhisperModel(model_size, device="cpu", compute_type="int8",
+                     local_files_only=True, download_root="/tmp")
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
+
 def allowed_file(filename: str):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/transcribe", methods=['POST', 'GET'])
 def transcribe():
@@ -27,16 +30,18 @@ def transcribe():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            
+
             # data = file.read()
 
             segments, info = model.transcribe(file, beam_size=5)
             res = []
             for segment in segments:
-                res.append("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-            
-            rest = "Detected language '%s' with probability %f" % (info.language, info.language_probability)
-            return  rest + "<br>" + "<br>".join(res)
+                res.append("[%.2fs -> %.2fs] %s" %
+                           (segment.start, segment.end, segment.text))
+
+            rest = "Detected language '%s' with probability %f" % (
+                info.language, info.language_probability)
+            return rest + "<br>" + "<br>".join(res)
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -46,6 +51,7 @@ def transcribe():
       <input type=submit value=Upload>
     </form>
     '''
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
